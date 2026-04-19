e8ff669c756846358e99d3bf9a9975b27f94eda4
rula11 <132067935+rula11@users.noreply.github.com>
2026-04-19 22:57:15 +0700

    app.py

--- app.py
+++ app.py
@@ -2,32 +2,48 @@ from flask import Flask, request, jsonify
 import onnxruntime as ort
 import numpy as np
 from PIL import Image
+import os
+import gdown
 
 app = Flask(__name__)
 
-# ======================
-# LOAD MODEL (ONNX)
-# ======================
 MODEL_PATH = "model.onnx"
+DATA_PATH = "model.onnx.data"
 
+# ======================
+# DOWNLOAD MODEL (AUTO)
+# ======================
+if not os.path.exists(MODEL_PATH):
+    print("Download ONNX...")
+    gdown.download(
+        "https://drive.google.com/uc?id=17Fa1tk2AiGwRtKbaEPDSNhAkJw8MZ7lg",
+        MODEL_PATH,
+        quiet=False
+    )
+
+if not os.path.exists(DATA_PATH):
+    print("Download DATA...")
+    gdown.download(
+        "https://drive.google.com/uc?id=1wuCtmbUzxG3D4QdRXKmVAE6yLlLs6U7_",
+        DATA_PATH,
+        quiet=False
+    )
+
+# load model
 session = ort.InferenceSession(MODEL_PATH)
 
 # ======================
-# PREPROCESS (SAMAKAN DENGAN TRAINING)
+# PREPROCESS
 # ======================
 def preprocess(image):
     image = image.resize((224, 224))
     image = np.array(image).astype(np.float32) / 255.0
 
-    # Normalize (sama seperti training kamu)
     mean = np.array([0.485, 0.456, 0.406])
     std  = np.array([0.229, 0.224, 0.225])
+
     image = (image - mean) / std
-
-    # HWC → CHW
     image = np.transpose(image, (2, 0, 1))
-
-    # tambah batch dimension
     image = np.expand_dims(image, axis=0)
 
     return image
@@ -43,15 +59,11 @@ def home():
 def predict():
     try:
         file = request.files["file"]
-
-        # baca gambar
         image = Image.open(file.stream).convert("RGB")
+
         input_data = preprocess(image)
 
-        # inference ONNX
         outputs = session.run(None, {"input": input_data})
-
-        # sigmoid manual (karena output masih logit)
         prob = 1 / (1 + np.exp(-outputs[0][0][0]))
 
         label = "High" if prob > 0.5 else "Normal"
@@ -66,8 +78,5 @@ def predict():
             "error": str(e)
         })
 
-# ======================
-# RUN
-# ======================
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
